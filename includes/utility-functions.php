9154629c24702069d165fdbd0b0d5af6298052b5
arielkeybob <arielkeybob@gmail.com>
2023-11-05 18:10:35 -0300

    Produtos sem meta_key da ordem não aparecendo na lista da página de edição da categoria

--- includes/utility-functions.php
+++ includes/utility-functions.php
@@ -4,25 +4,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-// Gets the list of products belonging to a category
 function get_category_products($category_id) {
     $args = array(
         'post_type' => 'product',
         'posts_per_page' => -1,
-        'post_status' => 'publish',
         'tax_query' => array(
             array(
                 'taxonomy' => 'product_cat',
-                'field' => 'id',
-                'terms' => $category_id,
-                'operator' => 'IN',
+                'field'    => 'term_id',
+                'terms'    => $category_id,
             ),
         ),
-        'orderby' => 'meta_value_num', // Ordenar por valor numérico da meta
-        'meta_key' => 'order_in_category_' . $category_id, // A chave da meta para ordenar
-        'order' => 'ASC' // Ordem ascendente
+        'meta_query' => array(
+            'relation' => 'OR',
+            array(
+                'key'     => 'order_in_category_' . $category_id,
+                'compare' => 'EXISTS',
+            ),
+            array(
+                'key'     => 'order_in_category_' . $category_id,
+                'compare' => 'NOT EXISTS',
+                'value'   => '' // Isso é apenas um placeholder.
+            ),
+        ),
     );
 
-    return get_posts($args);
+    $posts = get_posts($args);
+
+    // Ordenar os posts manualmente.
+    usort($posts, function ($a, $b) use ($category_id) {
+        $orderA = (int) get_post_meta($a->ID, 'order_in_category_' . $category_id, true);
+        $orderB = (int) get_post_meta($b->ID, 'order_in_category_' . $category_id, true);
+
+        if ($orderA === $orderB) {
+            return 0;
+        }
+
+        return ($orderA < $orderB) ? -1 : 1;
+    });
+
+    return $posts;
 }
 
