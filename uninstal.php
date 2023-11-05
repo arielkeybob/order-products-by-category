0b83aad23ab209d1a0cce98a1682d4aa3f86dbeb
arielkeybob <arielkeybob@gmail.com>
2023-11-05 11:58:07 -0300

    Corrigido unistall não sendo executado

--- uninstal.php
+++ uninstal.php
@@ -4,10 +4,15 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
+// Para segurança extra, verifique se o usuário tem permissão para deletar plugins antes de executar qualquer coisa
+if (!current_user_can('delete_plugins')) {
+    exit();
+}
+
 // Limpeza de dados: remove opções, tabelas e metadados
 
 // Opção para verificar se os dados devem ser removidos
-$should_cleanup = get_option('opbycat_cleanup_data_on_uninstall');
+$should_cleanup = get_option('opbycat_cleanup_data');
 
 // Remove metadados de produtos e opções apenas se o usuário optou por isso
 if ($should_cleanup === 'yes') {
@@ -15,7 +20,7 @@ if ($should_cleanup === 'yes') {
 
     // Remove opções
     delete_option('opbycat_enable_ordering');
-    delete_option('opbycat_cleanup_data_on_uninstall');
+    delete_option('opbycat_cleanup_data');
 
     // Remove metadados dos produtos
     // Substitua 'order_in_category_' pelo prefixo correto que você usou em seus metadados
@@ -35,9 +40,4 @@ if ($should_cleanup === 'yes') {
         ['meta_key' => $term_meta_key],
         ['%s']
     );
-}
-
-// Para segurança extra, verifique se o usuário tem permissão para deletar plugins antes de executar qualquer coisa
-if (!current_user_can('delete_plugins')) {
-    exit();
 }
