d9b8f18b928c7627217d307d1e2f354057af6a5e
arielkeybob <arielkeybob@gmail.com>
2023-11-06 02:40:28 -0300

    Refatorando utility-functions.php função de ordenação na tela de edição de categoria

--- includes/product-ordering.php
+++ includes/product-ordering.php
@@ -7,35 +7,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action('pre_get_posts', 'custom_order_by_order_value');
 
 function custom_order_by_order_value($query) {
-    // Verificar se a ordenação está ativada
-    $ordering_enabled = get_option('opbycat_enable_ordering') === 'yes';
+    // Apenas alterar a consulta principal no front-end e para categorias de produto
+    if (!is_admin() && $query->is_main_query() && is_product_category()) {
+        $ordering_enabled = get_option('opbycat_enable_ordering') === 'yes';
 
-    if (!is_admin() && $query->is_main_query() && is_product_category() && $ordering_enabled) {
-        $category_id = get_queried_object_id();
-        $product_ordering = get_term_meta($category_id, 'product_ordering', true);
+        // Continuar apenas se a ordenação estiver ativada
+        if ($ordering_enabled) {
+            $category_id = get_queried_object_id();
+            $product_ordering = get_term_meta($category_id, 'product_ordering', true);
 
-        if ($product_ordering === 'order_value') {
-            // Ordenar por order_value
-            $query->set('orderby', 'meta_value_num');
-            $query->set('meta_key', 'order_in_category_' . $category_id);
-            $query->set('order', 'ASC');
-            $query->set('meta_query', array(
-                'relation' => 'OR',
-                array(
-                    'key' => 'order_in_category_' . $category_id,
-                    'compare' => 'EXISTS',
-                ),
-                array(
-                    'key' => 'order_in_category_' . $category_id,
-                    'compare' => 'NOT EXISTS',
-                    'value' => '0',
-                ),
-            ));
-        } elseif ($product_ordering === 'title') {
-            // Ordenar por título
-            $query->set('orderby', 'title');
-            $query->set('order', 'ASC');
+            switch ($product_ordering) {
+                case 'order_value':
+                    // Ordenar por um valor meta personalizado
+                    $query->set('orderby', 'meta_value_num');
+                    $query->set('meta_key', 'order_in_category_' . $category_id);
+                    $query->set('order', 'ASC');
+                    $query->set('meta_query', array(
+                        'relation' => 'OR',
+                        array(
+                            'key' => 'order_in_category_' . $category_id,
+                            'compare' => 'EXISTS',
+                        ),
+                        array(
+                            'key' => 'order_in_category_' . $category_id,
+                            'compare' => 'NOT EXISTS',
+                            'value' => '0',
+                        ),
+                    ));
+                    break;
+                case 'title':
+                    // Ordenar por título
+                    $query->set('orderby', 'title');
+                    $query->set('order', 'ASC');
+                    break;
+                default:
+                    // Deixa a ordenação padrão do WordPress (não faz alterações)
+                    break;
+            }
         }
-        // A opção "default" mantém a ordenação padrão do WordPress
     }
 }
+add_action('pre_get_posts', 'custom_order_by_order_value');
+
--- includes/utility-functions.php
+++ includes/utility-functions.php
@@ -4,10 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-function get_category_products($category_id) {
+function get_category_products($category_id, $per_page = 10, $page_number = 1) {
     $args = array(
         'post_type' => 'product',
-        'posts_per_page' => -1,
+        'posts_per_page' => $per_page,
+        'paged' => $page_number,
         'tax_query' => array(
             array(
                 'taxonomy' => 'product_cat',
@@ -24,25 +25,32 @@ function get_category_products($category_id) {
             array(
                 'key'     => 'order_in_category_' . $category_id,
                 'compare' => 'NOT EXISTS',
-                'value'   => '' // Isso é apenas um placeholder.
+                'value'   => '' // Placeholder para produtos sem valor definido.
             ),
         ),
     );
 
+    // Permitir que os argumentos da consulta sejam modificados por outros desenvolvedores antes da consulta ser realizada
+    $args = apply_filters('opbycat_get_category_products_args', $args, $category_id);
+
+    // Busca os posts com os argumentos definidos
     $posts = get_posts($args);
 
-    // Ordenar os posts manualmente.
+    // Se nenhum produto for encontrado, retorne um array vazio ou um erro
+    if (empty($posts)) {
+        return apply_filters('opbycat_no_products_found', array(), $category_id);
+    }
+
+    // Ordenação manual dos posts com base no valor de meta personalizado
     usort($posts, function ($a, $b) use ($category_id) {
         $orderA = (int) get_post_meta($a->ID, 'order_in_category_' . $category_id, true);
         $orderB = (int) get_post_meta($b->ID, 'order_in_category_' . $category_id, true);
 
-        if ($orderA === $orderB) {
-            return 0;
-        }
-
-        return ($orderA < $orderB) ? -1 : 1;
+        return $orderA <=> $orderB; // Operador de comparação de nave espacial PHP 7
     });
 
-    return $posts;
+    // Permitir que o resultado final seja modificado por outros desenvolvedores antes de ser retornado
+    return apply_filters('opbycat_get_category_products', $posts, $category_id);
 }
+
 
