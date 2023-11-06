4e75a466712bc62328c278f5cfeba2ce652ad59a
arielkeybob <arielkeybob@gmail.com>
2023-11-06 01:23:58 -0300

    Refatorando admin-fields.php - exclusão do uso da variavel global

--- includes/admin-fields.php
+++ includes/admin-fields.php
@@ -1,22 +1,23 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
-
-// Verificar se a ordenação está ativada
-$ordering_enabled = get_option('opbycat_enable_ordering') === 'yes';
 
 // Adicionar um campo personalizado à página de edição de categorias para exibir produtos
 add_action('product_cat_edit_form_fields', 'display_category_products_field', 20); // Prioridade 20
 
 function display_category_products_field($term) {
-    global $ordering_enabled; // Usar a variável global definida acima
+    // Verificar se a ordenação está ativada
+    $ordering_enabled = get_option('opbycat_enable_ordering') === 'yes';
 
     // Se a ordenação não estiver ativada, não exibir os campos
     if (!$ordering_enabled) {
         echo '<tr class="form-field"><th scope="row" valign="top"><label>A ordenação personalizada está desativada.</label></th></tr>';
         return;
     }
+
+    // Nonce field para validação
+    wp_nonce_field('save_category_products_order', 'category_products_order_nonce');
 
     $category_id = $term->term_id;
     $products = get_category_products($category_id);
@@ -53,17 +54,36 @@ function display_category_products_field($term) {
     echo '</td></tr>';
 }
 
-// Save the custom numeric field values of products associated with this category
-add_action('edited_product_cat', 'save_category_products_order', 10, 2); // Priority 10
+
+
+// Salvar os valores dos campos personalizados de produtos associados a esta categoria
+add_action('edited_product_cat', 'save_category_products_order', 10, 2); // Prioridade 10
 
 function save_category_products_order($term_id, $tt_id) {
-    if (isset($_POST['order_in_category'])) {
+    // Verificar se o nonce está definido e é válido
+    if (!isset($_POST['category_products_order_nonce']) || !wp_verify_nonce($_POST['category_products_order_nonce'], 'save_category_products_order')) {
+        return; // Sair da função se o nonce não for válido
+    }
+
+    // Verificar se o usuário tem permissão para editar a categoria
+    if (!current_user_can('edit_term', $term_id)) {
+        return; // Sair da função se o usuário não tem permissão
+    }
+
+    // Verificar e salvar os valores do campo de ordenação dos produtos
+    if (isset($_POST['order_in_category']) && is_array($_POST['order_in_category'])) {
         foreach ($_POST['order_in_category'] as $product_id => $order_value) {
+            // Sanitizar os valores
+            $product_id = absint($product_id); // Assegurar que é um número inteiro
+            $order_value = sanitize_text_field($order_value); // Limpar o valor do campo de texto
+            // Atualizar o meta do post
             update_post_meta($product_id, 'order_in_category_' . $term_id, $order_value);
         }
     }
 
-    // Salvar a opção de ordenação selecionada
-    $product_ordering = isset($_POST['product_ordering']) ? sanitize_text_field($_POST['product_ordering']) : 'title';
+    // Sanitizar e salvar a opção de ordenação selecionada
+    $product_ordering = isset($_POST['product_ordering']) ? sanitize_text_field($_POST['product_ordering']) : 'default';
     update_term_meta($term_id, 'product_ordering', $product_ordering);
 }
+
+
